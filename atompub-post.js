c97e49c28e3886cec8768ba18f8a1717b96be4f9
angler <jofi5616@yahoo.co.jp>
2026-09-10 16:23:38 +0900

    20260910

--- atompub-post.js
+++ atompub-post.js
@@ -41,7 +41,7 @@ async function postArticle() {
 </entry>
 `;
 
-  const url = `https://livedoor.blogcms.jp/atom/${LD_USER}/article`;
+  const url = `https://livedoor.blogcms.jp/atom/beetle_life_jp_blog/article`;
 
   try {
     const res = await axios.post(url, xml, {
